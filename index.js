d34b7eabe1497c85276c2bfd8f91f22a9d14e6c8
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-21 19:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 // Add your Circle class here
-class Bird {
-  constructor(name){
-    this.name = name;
-  }
-  set phrase(phrase){
-    this._phrase = phrase;
+class Circle {
+  constructor(radius){
+    this.radius = radius;
   }
   
-  get speak(){
-    return `${this.name} says ${this._phrase || 'squawk'}`
+  pi(){
+    return Math.PI(this.radius)
   }
 }
-
-let daffy = new Bird("Daffy");
-daffy.speak
-daffy.phrase = "It's rabbit season"
-daffy.speak
